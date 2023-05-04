b4be6c167db9bf094c256e984ba2cfe6300565ca
LbAsHaDev_abdessamia_rguibi <abdessamia63@gmail.com>
2023-05-04 17:35:25 +0100

    imp

--- assets/js/main.js
+++ assets/js/main.js
@@ -158,3 +158,254 @@ function navHighlighter() {
 }
 
 /* Show Scroll Up */
+
+const { gsap, imagesLoaded } = window;
+
+const buttons = {
+  prev: document.querySelector(".btn--left"),
+  next: document.querySelector(".btn--right"),
+};
+const cardsContainerEl = document.querySelector(".cards__wrapper");
+
+const cardInfosContainerEl = document.querySelector(".info__wrapper");
+
+buttons.next.addEventListener("click", () => swapCards("right"));
+
+buttons.prev.addEventListener("click", () => swapCards("left"));
+
+function swapCards(direction) {
+  const currentCardEl = cardsContainerEl.querySelector(".current--card");
+  const previousCardEl = cardsContainerEl.querySelector(".previous--card");
+  const nextCardEl = cardsContainerEl.querySelector(".next--card");
+
+  changeInfo(direction);
+  swapCardsClass();
+
+  removeCardEvents(currentCardEl);
+
+  function swapCardsClass() {
+    currentCardEl.classList.remove("current--card");
+    previousCardEl.classList.remove("previous--card");
+    nextCardEl.classList.remove("next--card");
+
+    currentCardEl.style.zIndex = "50";
+
+    if (direction === "right") {
+      previousCardEl.style.zIndex = "20";
+      nextCardEl.style.zIndex = "30";
+      currentCardEl.classList.add("previous--card");
+      previousCardEl.classList.add("next--card");
+      nextCardEl.classList.add("current--card");
+    } else if (direction === "left") {
+      previousCardEl.style.zIndex = "30";
+      nextCardEl.style.zIndex = "20";
+      currentCardEl.classList.add("next--card");
+      previousCardEl.classList.add("current--card");
+      nextCardEl.classList.add("previous--card");
+    }
+  }
+}
+
+function changeInfo(direction) {
+  let currentInfoEl = cardInfosContainerEl.querySelector(".current--info");
+  let previousInfoEl = cardInfosContainerEl.querySelector(".previous--info");
+  let nextInfoEl = cardInfosContainerEl.querySelector(".next--info");
+
+  gsap
+    .timeline()
+    .to([buttons.prev, buttons.next], {
+      duration: 0.2,
+      opacity: 0.5,
+      pointerEvents: "none",
+    })
+    .to(
+      currentInfoEl.querySelectorAll(".text"),
+      {
+        duration: 0.4,
+        stagger: 0.1,
+        translateY: "-120px",
+        opacity: 0,
+      },
+      "-="
+    )
+    .call(() => {
+      swapInfosClass(direction);
+    })
+    .call(() => initCardEvents())
+    .fromTo(
+      direction === "right"
+        ? nextInfoEl.querySelectorAll(".text")
+        : previousInfoEl.querySelectorAll(".text"),
+      {
+        opacity: 0,
+        translateY: "40px",
+      },
+      {
+        duration: 0.4,
+        stagger: 0.1,
+        translateY: "0px",
+        opacity: 1,
+      }
+    )
+    .to([buttons.prev, buttons.next], {
+      duration: 0.2,
+      opacity: 1,
+      pointerEvents: "all",
+    });
+
+  function swapInfosClass() {
+    currentInfoEl.classList.remove("current--info");
+    previousInfoEl.classList.remove("previous--info");
+    nextInfoEl.classList.remove("next--info");
+
+    if (direction === "right") {
+      currentInfoEl.classList.add("previous--info");
+      nextInfoEl.classList.add("current--info");
+      previousInfoEl.classList.add("next--info");
+    } else if (direction === "left") {
+      currentInfoEl.classList.add("next--info");
+      nextInfoEl.classList.add("previous--info");
+      previousInfoEl.classList.add("current--info");
+    }
+  }
+}
+
+function updateCard(e) {
+  const card = e.currentTarget;
+  const box = card.getBoundingClientRect();
+  const centerPosition = {
+    x: box.left + box.width / 2,
+    y: box.top + box.height / 2,
+  };
+  let angle = Math.atan2(e.pageX - centerPosition.x, 0) * (35 / Math.PI);
+  gsap.set(card, {
+    "--current-card-rotation-offset": `${angle}deg`,
+  });
+  const currentInfoEl = cardInfosContainerEl.querySelector(".current--info");
+  gsap.set(currentInfoEl, {
+    rotateY: `${angle}deg`,
+  });
+}
+
+function resetCardTransforms(e) {
+  const card = e.currentTarget;
+  const currentInfoEl = cardInfosContainerEl.querySelector(".current--info");
+  gsap.set(card, {
+    "--current-card-rotation-offset": 0,
+  });
+  gsap.set(currentInfoEl, {
+    rotateY: 0,
+  });
+}
+
+function initCardEvents() {
+  const currentCardEl = cardsContainerEl.querySelector(".current--card");
+  currentCardEl.addEventListener("pointermove", updateCard);
+  currentCardEl.addEventListener("pointerout", (e) => {
+    resetCardTransforms(e);
+  });
+}
+
+initCardEvents();
+
+function removeCardEvents(card) {
+  card.removeEventListener("pointermove", updateCard);
+}
+
+function init() {
+  let tl = gsap.timeline();
+
+  tl.to(cardsContainerEl.children, {
+    delay: 0.15,
+    duration: 0.5,
+    stagger: {
+      ease: "power4.inOut",
+      from: "right",
+      amount: 0.1,
+    },
+    "--card-translateY-offset": "0%",
+  })
+    .to(
+      cardInfosContainerEl
+        .querySelector(".current--info")
+        .querySelectorAll(".text"),
+      {
+        delay: 0.5,
+        duration: 0.4,
+        stagger: 0.1,
+        opacity: 1,
+        translateY: 0,
+      }
+    )
+    .to(
+      [buttons.prev, buttons.next],
+      {
+        duration: 0.4,
+        opacity: 1,
+        pointerEvents: "all",
+      },
+      "-=0.4"
+    );
+}
+
+//sidebar
+
+const sidebar = document.querySelector(".sidebar");
+const navToggle = document.querySelector("#nav-toggle");
+const navClose = document.querySelector("#nav-close");
+let navOpen = false;
+
+navToggle.addEventListener("click", () => {
+  if (!navOpen) {
+    sidebar.classList.add("show-sidebar");
+    navOpen = true;
+  } else {
+    sidebar.classList.remove("show-sidebar");
+    navOpen = false;
+  }
+});
+
+navClose.addEventListener("click", () => {
+  if (navOpen) {
+    sidebar.classList.remove("show-sidebar");
+    navOpen = false;
+  }
+});
+
+const navItems = document.querySelectorAll(".nav_item");
+for (const item of navItems) {
+  item.addEventListener("click", () => {
+    if (navOpen) {
+      sidebar.classList.remove("show-sidebar");
+      navOpen = false;
+    }
+  });
+}
+
+//contact
+
+const contactUser = document.getElementById("contactUser");
+const contactPhone = document.getElementById("contactPhone");
+const contactEmail = document.getElementById("contactEmail");
+const contactMsg = document.getElementById("contactMsg");
+const contactSubmit = document.getElementById("contactSubmit");
+
+contactSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  contactClick();
+});
+
+const contactClick = () => {
+  Email.send({
+    SecureToken: "901754eb-35ce-40c9-9860-25850a65d771",
+    To: "abdessamia63@gmail.com",
+    From: "abdessamia63@gmail.com",
+    Subject: `Portfolio message from ${contactUser.value}`,
+    Body: `
+      <h1>Portfolio message from ${contactUser.value}</h1>
+      <h3>Phone: ${contactPhone.value}</h3>
+      <h3>Email: ${contactEmail.value}</h3>
+      <p>${contactMsg.value}</p>
+    `,
+  }).then((message) => console.log(message));
+};
